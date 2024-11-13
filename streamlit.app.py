9dead8d9391d56fe539e9758c2aa42b4eba943df
Abila940 <abila.r@mastek.com>
2024-11-13 11:39:22 +0530

    Update streamlit.app.py

--- streamlit.app.py
+++ streamlit.app.py
@@ -46,4 +46,4 @@ if time_to_insert:
 #New sectionto display smoothiefroot nutrition information
 import requests
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response).json())
+st.text(smoothiefroot_response).json()
